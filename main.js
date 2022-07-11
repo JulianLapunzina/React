8208c7f8e01d7aafdad65fe7c3d2bec5ee7ad795
Julian <julianlapunzina@gmail.com>
2022-07-11 18:09:43 -0300

    Desafío complementario n°2

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-
+/*
                                         // Constantes Globales
 const usuario1 = 'Pedro'
 const contrasenia1 = 'Pedro123'
@@ -102,7 +102,47 @@ let resultado = nombreDelLibro + ', ' + autor + ', ' + fechaDePublicacion + ', '
 alert('Los datos ingresados son: ' + resultado)
 
 
+*/
+
+/*
+                    // Desafío complementario N°2 - Julián Lapunzina
+
+
+        // Array de almacenamiento de usuarios + Clase con función constructora de Usuario
+
+const usuarios = []
+
+class Usuario {
+    constructor(nombreDeUsuario, contrasenia) {
+        this.nombreDeUsuario = nombreDeUsuario
+        this.contrasenia = contrasenia
+    }
+}
+
+const registrarUsuario = (nombredeUsuario, contrasenia) => {
+    nombreDeUsuario = prompt('Ingrese el nuevo nombre de Usuario')
+    contrasenia = prompt('Ingrese su nueva contraseña')
+    let usuarioNuevo = new Usuario (nombreDeUsuario, contrasenia)
+    usuarios.push(usuarioNuevo)
+}
+
+// Usuarios ya creados que se almacenan para verificar si coincide o no con los que ingresa el usuario en un futuro
+
+const usuarioRuben = new Usuario ("Ruben", 12345)
+usuarios.push(usuarioRuben)
+const ususarioFernanda = new Usuario ("Fernanda",12345)
+usuarios.unshift(ususarioFernanda)
+
+
+console.log(usuarios)
+
+// Registro de usuario
+let bienvenida = alert('Bienvenido a Vean, para comenzar necesitamos que cree un nuevo Usuario')
+registrarUsuario()
+
         // Clase con función constructora del libro + comisión de la plataforma igualado al iva
+
+alert('Perfecto, ahora comencemos a cargar los datos de su libro')
 
 class Libro {
     constructor(nombredelLibro, autor, fechaDePublicacion, precio, categoria,id) {
@@ -118,9 +158,37 @@ class Libro {
     }
 }
 
+// Array de almacenamiento de Libros + Función que pide los datos y construye el libro
 
-// Debajo pongo un ejemplo de cómo crear un nuevo Libro, pasandole los parámetros 
-const Libro1 = new Libro("Game of Thrones", "George Martin", "06/08/1996", 4700, 3, 33064675)
-Libro1.comision()
-console.log(Libro1)
+let Libros = []
 
+const registrarLibro = () => {
+    let nombreDelLibro = prompt('Ingrese el nombre del libro')
+    let autor = prompt('Ingrese el nombre de autoría que desea registar')
+    let fechaDePublicacion = prompt('Ingrese la fecha de publicación del libro')
+    let precio = parseFloat(prompt('Ingrese el valor del libro'))
+    let categoria = prompt('Ingrese en qué categoría se encuentra su libro: cuento, novela, poesía, otros' )
+    let id = prompt('Ingrese su número de documento para registrar el libro')
+    let libroNuevo = new Libro (nombreDelLibro, autor, fechaDePublicacion, precio, categoria, id)
+    Libros.push(libroNuevo)
+    Libros.join(',')
+    let accion = confirm ('¡Felicitaciones! Su libro ha sido registrado exitosamente. ¿Desea verificar los datos? Si continua, al finalizar, por favor acepte en caso de que sus datos sean correctos o cancele para volver a realizar el proceso nuevamente')
+    //el condicional de abajo lo que hace es que si el usuario confirma que los datos que ingresó están mal, vuelve a llamar a la función para que pueda agregarlos nuevamente, y además elimina los datos que ingresó viejos.
+    if(accion != false) {
+        let accion2 = confirm(JSON.stringify(libroNuevo,null,3));
+        if(accion2 != true) {
+            Libros.splice(0,7)
+            registrarLibro()
+        }
+    }
+    
+}
+
+registrarLibro ()
+
+console.log(Libros)
+
+const librosyusuarios = usuarios.concat(Libros)
+console.log(librosyusuarios)
+
+*/
